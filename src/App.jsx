f77077d59190209710042754c5841f59dd567711
Nargis <nargissamatova16@gmail.com>
2024-08-30 09:20:40 +0300

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import sinister from "./assets/sinister.mp3";
+import stomping from "./assets/stomp.mp3";
+import underwater from "./assets/underwater.mp3";
+import { PlayerControls } from "./components/PlayerControls";
+import { TrackList } from "./components/TrackList";
+import { MusicContext } from "./contexts/MusicContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState({
+    audioPlayer: new Audio(),
+    currentTrackIndex: 0,
+    isPlaying: false,
+    tracks: [
+      {
+        name: "Sinister Music ...",
+        file: sinister,
+      },
+      {
+        name: "Stomp - Stomping!!!",
+        file: stomping,
+      },
+      {
+        name: "Underwater ... bubbles",
+        file: underwater,
+      },
+    ],
+  });
 
   return (
-    <>
+    <MusicContext.Provider value={[state, setState]}>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <TrackList />
+        <PlayerControls />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </MusicContext.Provider>
+  );
 }
 
-export default App
+export default App;
